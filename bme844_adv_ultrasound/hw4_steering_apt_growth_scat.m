3c97d4fb536d1335dc23796aabe9095cf8c367cc
Willie Long <Donatello@Wills-Macbook.local>
2015-02-05 00:27:54 -0500

    bme844 hw4 final revision

--- bme844_adv_ultrasound/hw4_steering_apt_growth_scat.m
+++ bme844_adv_ultrasound/hw4_steering_apt_growth_scat.m
@@ -24,6 +24,10 @@ focus=[0 0 elv_focus]; %focus at 4 cm depth
 maxth = 60;
 th_scan = [-5:0.1:maxth]*pi/180;
 th_target = [0:10:maxth-10]*pi/180;
+
+% maxth = 70;
+% th_scan = [-5:0.1:maxth]*pi/180;
+% th_target = [0:5:maxth-10]*pi/180;
 foc_z = focus(3);
 
 N_el_tot = 128;
@@ -53,6 +57,7 @@ for n = 1:2
                     fill = N_el_tot-2*pad-length(tmp2);
                     apod = [padarray(tmp2,[0,pad]) zeros(1,fill)];
                 end
+                clear tmp1 tmp2
                 el(nn) = length(find(apod == 1));
                 Fnum(nn) = foc_z/(el(nn)*el_width*cos(th_scan(nn)));
 
@@ -91,18 +96,19 @@ for n = 1:2
         idx = find(r{n,i}*100>3.8 & r{n,i}*100<4.2);
         env_trim{n,i} = env{n,i}(idx,:);
         rf_trim{n,i} = rf{n,i}(idx,:);
-
         
+        tmp1 = diff(r{n,i});
+        tmp2 = diff(th_scan);
+        T_y(n,i) = 1000*tmp1(1);
+        T_x(n,i) = tmp2(1);
+        clear tmp1 tmp2
+        k_y{n,i} = (1/(2*T_y(n,i)))*linspace(-1,1,length(idx));
+        k_x{n,i} = (1/(2*T_x(n,i)))*linspace(-1,1,length(th_scan));
+
         env_trim_k{n,i} = fftshift(fft2(env_trim{n,i}));
         rf_trim_k{n,i} = fftshift(fft2(rf_trim{n,i}));
-        
-    %     figure
-    % %     imagesc(l,ax,out,[-40 0]); axis image; colormap gray;
-    %     imagesc(th_scan,r{i}(idx)*100,env_trim{i}); axis image; colormap jet;
 
-    %     figure
-    %     imagesc(th_scan,r{i}(idx)*100,rf_trim{i}); axis image; colormap jet;
-    %     
+        % calculate correlation via xcorr in spatial domain with kernel
         if th_target(i) == 0
             k_i = find(r{n,i}*100>4 & r{n,i}*100<4.15);
             k_j = find(th_scan>-0.05 & th_scan<0.05);
@@ -110,25 +116,18 @@ for n = 1:2
             rf_kernel = rf{n,i}(k_i,k_j);
         end
 
-    %     figure
-    %     imagesc(env_kernel)
-
-    %     figure
-    %     imagesc(rf_kernel)
-
         C_env{n,i} = normxcorr2(env_kernel,env_trim{n,i});
         C_rf{n,i} = normxcorr2(rf_kernel,rf_trim{n,i});
 
         Cmax_env(n,i) = max(C_env{n,i}(:));
         Cmax_rf(n,i) = max(C_rf{n,i}(:));
 
-    %     figure, imagesc(C_env{i}), colormap jet, colorbar
-    %     figure, imagesc(C_rf{i}), colormap jet, colorbar
-    %     figure
-    %     imagesc(th_scan,r{i}*100,C_env{i}); axis image; colormap jet; colorbar;
-    %     
-    %     figure
-    %     imagesc(th_scan,r{i}*100,C_rf{i}); axis image; colormap jet; colorbar;
+        % calculate correlation via normalized k-space product
+        kC_env(n,i) = sum(sum(abs(env_trim_k{n,1}).*abs(env_trim_k{n,i})))/...
+            sqrt(sum(sum(abs(env_trim_k{n,1}).^2))*sum(sum(abs(env_trim_k{n,i}).^2)));
+        kC_rf(n,i) = sum(sum(abs(rf_trim_k{n,1}).*abs(rf_trim_k{n,i})))/...
+            sqrt(sum(sum(abs(rf_trim_k{n,1}).^2))*sum(sum(abs(rf_trim_k{n,i}).^2)));
+        
         disp(['Image at angle ' num2str(rad2deg(th_target(i))) ' simulated.'])
     end
     xdc_free(tx); xdc_free(rx);
@@ -147,26 +146,88 @@ print -dpng -r300 ./hw4_aperture.png
 figure, hold on;
 p1 = plot(rad2deg(th_target), Cmax_env(1,:),'bx-','linewidth',2)
 p2 = plot(rad2deg(th_target), Cmax_env(2,:),'rx-','linewidth',2)
-grid on
+grid on,ylim([0.90 1])
 hold off
 legend([p1 p2],{'Aperture growth','Fixed aperture'},'fontsize',12,'location','southwest')
 xlabel('Angle (deg)','fontsize',12)
-ylabel('Normalized Correlation Coefficient','fontsize',12)
-title('PSF Decorrlation (Envelope Detected)','fontsize',12)
+ylabel('Correlation','fontsize',12)
+title('PSF Decorrelation (Envelope Detected)','fontsize',12)
 
 print -dpng -r300 ./hw4_psf_corr_env.png
 
 figure, hold on;
 p3 = plot(rad2deg(th_target), Cmax_rf(1,:),'bx-','linewidth',2)
 p4 = plot(rad2deg(th_target), Cmax_rf(2,:),'rx-','linewidth',2)
-grid on
+grid on, ylim([0.90 1])
 hold off
 legend([p3 p4],{'Aperture growth','Fixed aperture'},'fontsize',12,'location','southwest')
 xlabel('Angle (deg)','fontsize',12)
-ylabel('Normalized Correlation Coefficient')
+ylabel('Correlation')
 title('PSF Decorrelation (RF)','fontsize',12)
 
 print -dpng -r300 ./hw4_psf_corr_rf.png
+
+figure, hold on;
+p1 = plot(rad2deg(th_target), kC_env(1,:),'bx-','linewidth',2)
+p2 = plot(rad2deg(th_target), kC_env(2,:),'rx-','linewidth',2)
+grid on,ylim([0.90 1])
+hold off
+legend([p1 p2],{'Aperture growth','Fixed aperture'},'fontsize',12,'location','southwest')
+xlabel('Angle (deg)','fontsize',12)
+ylabel('Correlation','fontsize',12)
+title('PSF Decorrelation (Envelope Detected)','fontsize',12)
+
+print -dpng -r300 ./hw4_psf_corr_env_kspace.png
+
+figure, hold on;
+p3 = plot(rad2deg(th_target), kC_rf(1,:),'bx-','linewidth',2)
+p4 = plot(rad2deg(th_target), kC_rf(2,:),'rx-','linewidth',2)
+grid on, ylim([0.90 1])
+hold off
+legend([p3 p4],{'Aperture growth','Fixed aperture'},'fontsize',12,'location','southwest')
+xlabel('Angle (deg)','fontsize',12)
+ylabel('Correlation')
+title('PSF Decorrelation (RF)','fontsize',12)
+
+print -dpng -r300 ./hw4_psf_corr_rf_kspace.png
+
+
+ylims = [-10 10];
+xlims = [-100 100];
+
+figure
+subplot(131)
+imagesc(k_x{1,1},k_y{1,1},abs(rf_trim_k{1,1})); colormap jet; 
+ylim(ylims), xlim(xlims)
+xlabel('k\theta (1/rads)','fontsize',12), ylabel('kr (1/mm)','fontsize',12)
+title(['K-space ' num2str(rad2deg(th_target(1))) ' deg (Aperture Growth)'],'fontsize',12)
+subplot(132)
+imagesc(k_x{1,2},k_y{1,2},abs(rf_trim_k{1,2})); colormap jet; 
+ylim(ylims), xlim(xlims)
+xlabel('k\theta (1/rads)','fontsize',12), ylabel('kr (1/mm)','fontsize',12)
+title(['K-space ' num2str(rad2deg(th_target(2))) ' deg (Aperture Growth)'],'fontsize',12)
+subplot(133)
+imagesc(k_x{1,end},k_y{1,end},abs(rf_trim_k{1,end})); colormap jet; 
+ylim(ylims), xlim(xlims)
+xlabel('k\theta (1/rads)','fontsize',12), ylabel('kr (1/mm)','fontsize',12)
+title(['K-space ' num2str(rad2deg(th_target(end))) ' deg (Aperture Growth)'],'fontsize',12)
+
+figure
+subplot(131)
+imagesc(k_x{2,1},k_y{2,1},abs(rf_trim_k{2,1})); colormap jet;
+ylim(ylims), xlim(xlims)
+xlabel('k\theta (1/rads)','fontsize',12), ylabel('kr (1/mm)','fontsize',12)
+title(['K-space ' num2str(rad2deg(th_target(1))) ' deg (Fixed Aperture)'],'fontsize',12)
+subplot(132)
+imagesc(k_x{2,2},k_y{2,2},abs(rf_trim_k{2,2})); colormap jet; 
+ylim(ylims), xlim(xlims)
+xlabel('k\theta (1/rads)','fontsize',12), ylabel('kr (1/mm)','fontsize',12)
+title(['K-space ' num2str(rad2deg(th_target(2))) ' deg (Fixed Aperture)'],'fontsize',12)
+subplot(133)
+imagesc(k_x{2,end},k_y{2,end},abs(rf_trim_k{2,end})); colormap jet; 
+ylim(ylims), xlim(xlims)
+xlabel('k\theta (1/rads)','fontsize',12), ylabel('kr (1/mm)','fontsize',12)
+title(['K-space ' num2str(rad2deg(th_target(end))) ' deg (Fixed Aperture)'],'fontsize',12)
 
 field_end
 
